b595402ea2e47ea1f02084496442a7ddcc139de5
anuroo <anuroop72@gmail.com>
2022-08-28 08:22:47 +0530

    first commit

---
+++ Hybrid_Framework/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Hybrid_Framework")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Hybrid_Framework")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8cf37a10-80e8-4069-a9e3-3d7cdb0d2dc1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Hybrid_Framework/UnitTest1.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OfficeOpenXml;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Hybrid_Framework
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [DynamicData(nameof(ReadExcel), DynamicDataSourceType.Method)]
+        //Getdata is a method that is defined by the user
+        [TestMethod]
+        public void DatadrivenusingExcelSheet(string Email, string Password)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Url = "https://www.pggoodeveryday.com/login/";
+
+            driver.FindElement(By.Id("login-email")).SendKeys(Email);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(50);
+            driver.FindElement(By.Id("login-password")).SendKeys(Password);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(50);
+            // driver.FindElement(By.Id("EnrollmentDate")).SendKeys(eDate);
+            driver.FindElement(By.XPath("//input[@type='submit']")).Click();
+            Thread.Sleep(3000);
+            driver.Quit();
+        }
+        public static IEnumerable<object[]> ReadExcel()
+        {
+            //creating the worksheet object
+            using (ExcelPackage package = new ExcelPackage(new System.IO.FileInfo("test_data.xlsx")))
+            {
+                //worksheet object
+                ExcelWorksheet worksheet = package.Workbook.Worksheets["sheet1"];
+                int rowcount = worksheet.Dimension.End.Row;//give number of rows in integer form
+                for (int i = 2; i <= rowcount; i++)
+                {
+                    yield return new object[]
+                    {
+                        worksheet.Cells[i,1].Value?.ToString().Trim(),
+                        worksheet.Cells[i,2].Value?.ToString().Trim(),
+                    };
+                }
+            }
+        }
+    }
+}
